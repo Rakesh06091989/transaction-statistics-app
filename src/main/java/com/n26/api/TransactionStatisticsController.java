4b5987559b0b39f3195e9e8fe50a1ffc7a4f2c3c
Rakesh Thumar <rakesh.thumar@gmail.com>
2019-03-03 11:05:00 +0000

    comments changed

--- src/main/java/com/n26/api/TransactionStatisticsController.java
+++ src/main/java/com/n26/api/TransactionStatisticsController.java
@@ -35,7 +35,7 @@ public class TransactionStatisticsController {
     }
 
     /**
-     * This method will get transaction statistics
+     * This method will get transaction statistics of last five minutes
      * @return
      * @throws TransactionException
      */
